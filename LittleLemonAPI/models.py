8b0eaa00a85920024b1f4e3b6cdd8f51d5e5f278
Ahmed Gouda <ahmedmosad405@gmail.com>
2024-11-23 16:03:52 +0200

    add the models for the project and update the quantity fields in the UML diagram

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(db_index=True)
+    slug = models.SlugField()
+    
+class MenuItem(models.Model):
+    title = models.CharField(db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    featured = models.BooleanField(db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    class Meta:
+        unique_together = ('user', 'menuitem')
+        
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    status = models.BooleanField(db_index=True)
+    date = models.DateField(db_index=True)
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('order', 'menuitem')
+
