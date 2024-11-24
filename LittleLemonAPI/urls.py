88b146b75faf4728bb3a106ee5e9ad6272654220
Ahmed Gouda <ahmedmosad405@gmail.com>
2024-11-24 07:28:05 +0200

    add JWT token obtain and refresh endpoints to URL patterns

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import MenuItemList, CategoryList, CategoryDetail, MenuItemDetail
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('menu-items', MenuItemList.as_view(), name="menuitem"),
@@ -7,4 +8,6 @@ urlpatterns = [
     path('categories', CategoryList.as_view(), name="category"),
     path('categories/<int:pk>', CategoryDetail.as_view(), name="category-detail"),
     path('', include('djoser.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
