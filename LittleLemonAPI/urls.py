f9be7e27bbf7620eb6ef690ebdd63800204cf657
Ahmed Gouda <ahmedmosad405@gmail.com>
2024-11-24 07:34:44 +0200

    move JWT token end points to the root project end point

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path, include
 from .views import MenuItemList, CategoryList, CategoryDetail, MenuItemDetail
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('menu-items', MenuItemList.as_view(), name="menuitem"),
-    path('menu-items/<int:pk>', MenuItemDetail.as_view(), name="menuitem-detail"),
-    path('categories', CategoryList.as_view(), name="category"),
-    path('categories/<int:pk>', CategoryDetail.as_view(), name="category-detail"),
+    path('menu-items/', MenuItemList.as_view(), name="menuitem"),
+    path('menu-items/<int:pk>/', MenuItemDetail.as_view(), name="menuitem-detail"),
+    path('categories/', CategoryList.as_view(), name="category"),
+    path('categories/<int:pk>/', CategoryDetail.as_view(), name="category-detail"),
     path('', include('djoser.urls')),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
